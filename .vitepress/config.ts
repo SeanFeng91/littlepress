6cb0351a7f1c5e6aeda10a6df540adb3644f1a02
SeanFeng91 <fengyx91@gmail.com>
2025-06-24 16:58:17 +0800

    修改

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,16 +7,18 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '主页 / Home', link: '/' },
+      { text: '活动邀请 / Invitation', link: '/invitation/episode-01.md' },
+      { text: '活动总结 / Summary', link: '/last-episode.md' },
+      { text: '关于项目 / About', link: '/introduce.md' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: '活动邀请',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: '活动邀请', link: '/invitation/episode-01.md' },
+          { text: '活动总结', link: '/last-episode.md' }
         ]
       }
     ],
