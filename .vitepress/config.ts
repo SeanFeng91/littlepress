e18f4e77b00a393c7de602fbb68ad77648444b99
SeanFeng91 <fengyx91@gmail.com>
2025-06-24 17:17:11 +0800

    修改

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -8,20 +8,35 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '主页 / Home', link: '/' },
-      { text: '活动邀请 / Invitation', link: '/invitation/episode-01.md' },
-      { text: '活动总结 / Summary', link: '/last-episode.md' },
+      { text: '活动邀请 / Invitation', link: '/invitation/invitation.md' },
+      { text: '活动总结 / Summary', link: '/summary/summary.md' },
       { text: '关于项目 / About', link: '/introduce.md' }
     ],
-
-    sidebar: [
+    outline: {
+      level: [1, 6], // 显示 h2 到 h4 的标题
+      label: '本页目录'
+    },
+    sidebar: {
+    '/invitation/': [
       {
         text: '活动邀请',
         items: [
-          { text: '活动邀请', link: '/invitation/episode-01.md' },
-          { text: '活动总结', link: '/last-episode.md' }
+          { text: '第一期', link: '/invitation/episode-01.md' },
+          { text: '第二期', link: '/invitation/episode-02.md' }
         ]
-      }
+      },
     ],
+    '/summary/': [
+      {
+        text: '活动总结',
+        items: [
+          { text: '第一期', link: '/summary/episode-01.md' },
+          { text: '第二期', link: '/summary/episode-02.md' }
+        ]
+      },
+    ]
+    },
+    
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
